094c634e435d60b924de5737714fe200cf883195
Juan Cañar <jg.canarr@uea.edu.ec>
2025-01-07 19:54:47 -0500

    semana 3

---
+++ Desarrollo de Ejemplos de Técnicas de Programación (POO) copy.py
@@ -0,0 +1,37 @@
+#creacion de la clase
+class animal :
+    pass
+class perro(animal):
+    def nombre (self):
+        print("boby")
+    def tipo_animal(self):
+        print("carnivoro")
+class gato(animal):
+    def nombre(self):
+        print("lolo")
+    def tipo_animal(self):
+        print("carnivoro")
+animal1 = perro()
+animal2 = gato()
+print(animal1.nombre())
+print(animal1.tipo_animal())
+print(animal2.nombre())
+print(animal2.tipo_animal())
+class animal :
+    pass
+class perro(animal):
+    def nombre (self):
+        print("boby")
+    def tipo_animal(self):
+        print("carnivoro")
+class gato(animal):
+    def nombre(self):
+        print("lolo")
+    def tipo_animal(self):
+        print("carnivoro")
+animal1 = perro()
+animal2 = gato()
+print(animal1.nombre())
+print(animal1.tipo_animal())
+print(animal2.nombre())
+print(animal2.tipo_animal())
